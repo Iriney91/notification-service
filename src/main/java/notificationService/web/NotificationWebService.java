6000c0097ddc3f1bcbb64d79fbd4736c0972b1fd
Irina <Iriney_91@mail.ru>
2019-07-02 16:07:04 +0300

    web service

--- src/main/java/notificationService/web/NotificationWebService.java
+++ src/main/java/notificationService/web/NotificationWebService.java
@@ -7,24 +7,41 @@ import notificationService.model.PushNotification;
 import notificationService.service.NotificationService;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import java.util.Date;
 import java.util.List;
 
-@WebService(name = "NotificationWebService", targetNamespace = "http://www.itfbgroup.ru/notificationWebService")
+@WebService(serviceName = "NotificationWebService", portName = "NotificationWebPort", targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service")
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 
 public class NotificationWebService {
     NotificationService notificationService = NotificationService.getInstance();
+
     @WebMethod
-    public List<Message> receiveMessages(ChannelKind channelKind) {
-        return notificationService.receiveMessages(ChannelKind.TELEGRAM);
+    @WebResult(partName = "receiveMessagesResponse", targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service")
+    public List<Message> receiveMessages(@WebParam(partName = "receiveMessagesChannelRequest",
+            targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service") ChannelKind channelKind) {
+        return notificationService.receiveMessages(channelKind);
     }
+
+
     @WebMethod
-    public Message receiveMessage(String id, ChannelKind channelKind) {
-        return notificationService.receiveMessage("78668d0c-557d-4902-b55e-742212656715", ChannelKind.TELEGRAM);
+    @WebResult(partName = "receiveMessagesResponse", targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service")
+    public Message receiveMessage(@WebParam(
+            name = "id",
+            partName = "receiveMessageIdRequest",
+            targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service") String id,
+                                  @WebParam(
+                                          name = "channel",
+                                          partName = "receiveMessageChannelRequest",
+                                          targetNamespace = "http://www.itfbgroup.ru/telecom/notification-service") ChannelKind channelKind) {
+
+        return notificationService.receiveMessage(id, channelKind);
     }
+
     @WebMethod
     public PushNotification sendMessage(PushNotification message) {
         PushNotification push = new PushNotification("1", "Hello", new Date());
